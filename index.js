cfb3876975d6835893e9f41887b1fe47c1e55519
Vladimir Fursov <vladimirfursov228@gmail.com>
2023-01-15 17:57:01 +0200

    added middleware

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import { validationResult } from 'express-validator';
 import { registerValidation } from './validations/auth.js';
 
 import UserModel from './models/User.js';
+import checkAuth from './utils/сheckAuth.js';
 
 mongoose.set('strictQuery', true);
 
@@ -106,11 +107,25 @@ app.post('/auth/register', registerValidation, async (req, res) => {
   }
 });
 
-app.get('/auth/me', (req, res) => {
+app.get('/auth/me', checkAuth, async (req, res) => {
   try {
 
-  } catch (err) {
+    const user = await UserModel.findById(req.userId);
+
+    if (!user) {
+      return res.status(404).json({
+        message: 'User not found!'
+      });
+    }
     
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json(userData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'No access!',
+    })
   }
 });
 
